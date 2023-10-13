7a2d1650db643924cca6f03ea75e0d815faf063c
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-12 23:40:10 -0700

    refactor: rm python

--- scrape.py
+++
@@ -1,8 +0,0 @@
-from utils.scrape_br import BrScraper
-import time
-
-# Scrape will execute every 24 hours
-time_interval = 86400
-while True:
-    BrScraper.scrape_br()
-    time.sleep(time_interval)
